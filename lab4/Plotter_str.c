b965acd858a7be406f7c85c723f975619039f75a
nida242004 <syedanidafathima1@gmail.com>
2024-09-01 16:05:52 +0530

    string matching made easier

--- lab4/Plotter_str.c
+++ lab4/Plotter_str.c
@@ -1,80 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int count = 0;
-
-int stringmatching(char *text, char *pattern, int n, int m) {
-    count = 0;
-    for (int i = 0; i <= n - m; i++) {
+int strMatch(char *str, char *pattern, int m, int n)
+{
+    int count = 0;
+    for (int i = 0; i < m - n + 1; i++)
+    {
         int j = 0;
-        while (j < m) {
+        while (j < n && str[i + j] == pattern[j])
+        {
             count++;
-            if (pattern[j] != text[i + j]) {
-                break;
-            }
             j++;
         }
-        if (j == m) {
+        if (j == n)
             return count;
-        }
+        count++;
     }
     return count;
 }
 
-void plotter() {
-    FILE *f1 = fopen("stringbest.txt", "a");
-    FILE *f2 = fopen("stringworst.txt", "a");
-    FILE *f3 = fopen("stringavg.txt", "a");
+void main()
+{
+    FILE *fp;
+    fp = fopen("str.dat", "a");
+    int m = 1000;
+    char str[1000];
+    for (int i = 0; i < m; i++)
+        str[i] = 'a';
+    for (int i = 100; i <= m; i += 100)
+    { // i represents size of pattern
 
-    char *text = (char *)malloc(1000 * sizeof(char));
-    for (int i = 0; i < 1000; i++) {
-        text[i] = 'a'; // Initialize the text array with 'a'
+        fprintf(fp, "%d\t", i);
+        char pattern[i];
+        for (int j = 0; j < i; j++)
+            pattern[j] = 'a';
+
+        // Best Case
+        fprintf(fp, "%d\t", strMatch(str, pattern, m, i));
+
+        // Worst Case
+        pattern[i - 1] = 'b';
+        fprintf(fp, "%d\n", strMatch(str, pattern, m, i));
     }
-
-    int m = 10, n = 1000;
-
-    while (m <= 1000) {
-        char *pattern = (char *)malloc(m * sizeof(char));
-
-        // Best Case: All characters match
-        for (int i = 0; i < m; i++) {
-            pattern[i] = 'a';
-        }
-        count = 0;
-        stringmatching(text, pattern, n, m);
-        fprintf(f1, "%d\t%d\n", m, count);
-
-        // Worst Case: Last character differs
-        pattern[m - 1] = 'b';
-        count = 0;
-        stringmatching(text, pattern, n, m);
-        fprintf(f2, "%d\t%d\n", m, count);
-
-        // Average Case: Random characters
-        for (int i = 0; i < m; i++) {
-            pattern[i] = 'a' + rand() % 3; // Random 'a', 'b', or 'c'
-        }
-        count = 0;
-        stringmatching(text, pattern, n, m);
-        fprintf(f3, "%d\t%d\n", m, count);
-
-        free(pattern);
-
-        if (m < 100) {
-            m += 10;
-        } else {
-            m += 100;
-        }
-    }
-
-    free(text);
-    fclose(f1);
-    fclose(f2);
-    fclose(f3);
-}
-
-int main() {
-    plotter();
-    return 0;
+    fclose(fp);
 }
